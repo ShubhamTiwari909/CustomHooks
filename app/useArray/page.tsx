26010d9cb77c8ba8ee29f47cf5ae5be35ad43f78
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2023-10-30 18:37:22 +0530

    added navbar and removed the visuals for mobile and tablet and make it desktop only page

---
+++ app/useArray/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+import UseArray from "../components/UseArray"
+const page = () => {
+  return (
+   <UseArray />
+  )
+}
+
+export default page
---
+++ app/useToggle/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+import UseToggle from "../components/UseToggle"
+const page = () => {
+  return (
+   <UseToggle />
+  )
+}
+
+export default page
